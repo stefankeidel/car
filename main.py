6820da6caaf7f837807ad60a295199159e99272c
Stefan <stefan@whirl-i-gig.com>
2016-03-07 22:38:26 -0500

    Some general cleanup and try intake temp and fuel level for the next trial run tmrw

--- main.py
+++ main.py
@@ -10,54 +10,38 @@ import random
 class Dashboard(Widget):
     myobd = obd.OBD()
 
-    # row 1: speed, rpm, fuel rate
     spd = NumericProperty(0)
     rpm = NumericProperty(0)
     fuel_rate = NumericProperty(0)
-
-    cmd_spd = obd.commands.SPEED
-    cmd_rpm = obd.commands.RPM
-    cmd_fuel_rate = obd.commands.FUEL_RATE
-
-    # row 2: oil, cooland, air tmps
-    oil_tmp = NumericProperty(0)
+    fuel_level = NumericProperty(0)
     coolant_tmp = NumericProperty(0)
-    air_tmp = NumericProperty(0)
-
-    cmd_oil_tmp = obd.commands[1][92]
-    cmd_coolant_tmp = obd.commands.COOLANT_TEMP
-    cmd_air_tmp = obd.commands[1][70]
+    intake_tmp = NumericProperty(0)
 
     def update(self, dt):
-    	potential_spd = self.myobd.query(self.cmd_spd).value
-    	if potential_spd == None:
-    		potential_spd = 0   
-        self.spd = round(potential_spd * 0.621371,2)
+    	potential_spd = self.myobd.query(obd.commands.SPEED).value
+        potential_spd = 0 if potential_spd == None
+        self.spd = round(potential_spd * 0.621371, 2)
 
-    	potential_rpm = self.myobd.query(self.cmd_rpm).value
-    	if potential_rpm == None:
-    		potential_rpm = 0   
+    	potential_rpm = self.myobd.query(obd.commands.RPM).value
+        potential_rpm = 0 if potential_rpm == None
         self.rpm = potential_rpm
 
         potential_maf = self.myobd.query(obd.commands.MAF).value
-    	if potential_maf == None:
-    		potential_maf = 1
-        self.fuel_rate = round(self.spd * (7.718 / potential_maf),2)
+    	potential_maf = 1 if potential_maf == None # no division by 0
+        # instant fuel rate
+        self.fuel_rate = round(self.spd * (7.718 / potential_maf), 2)
 
-        potential_oil_tmp = self.myobd.query(self.cmd_oil_tmp).value
-    	if potential_oil_tmp == None:
-    		potential_oil_tmp = 0   
-        self.oil_tmp = potential_oil_tmp
-
-        potential_coolant_tmp = self.myobd.query(self.cmd_coolant_tmp).value
-    	if potential_coolant_tmp == None:
-    		potential_coolant_tmp = 0   
+        potential_coolant_tmp = self.myobd.query(obd.commands.COOLANT_TEMP).value
+        potential_coolant_tmp = 0 if potential_coolant_tmp == None
         self.coolant_tmp = potential_coolant_tmp
 
-        potential_air_tmp = self.myobd.query(self.cmd_air_tmp).value
-    	if potential_air_tmp == None:
-    		potential_air_tmp = 0   
-        self.air_tmp = potential_air_tmp
+        potential_intake_tmp = self.myobd.query(obd.commands.INTAKE_TEMP).value
+        potential_intake_tmp = 0 if potential_intake_tmp == None
+        self.intake_tmp = potential_intake_tmp
+
+        potential_fuel_level = self.myobd.query(obd.commands.FUEL_LEVEL).value
+        potential_fuel_level = 0 if potential_fuel_level == None
+        self.fuel_level = potential_fuel_level
 
 class DashboardApp(App):
     def build(self):
@@ -68,4 +52,3 @@ class DashboardApp(App):
 
 if __name__ == '__main__':
     DashboardApp().run()
-
