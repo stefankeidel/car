fbbad45deba6dc985ac78260f5ee3f7787625dd8
Stefan <stefan@whirl-i-gig.com>
2016-03-08 19:14:36 -0500

    Fix syntax errors. Man, python is weird

--- main.py
+++ main.py
@@ -18,30 +18,42 @@ class Dashboard(Widget):
     intake_tmp = NumericProperty(0)
 
     def update(self, dt):
-    	potential_spd = self.myobd.query(obd.commands.SPEED).value
-        potential_spd = 0 if potential_spd == None
-        self.spd = round(potential_spd * 0.621371, 2)
+        potential_spd = self.myobd.query(obd.commands.SPEED).value
+        if potential_spd == None:
+            self.spd = 0
+        else:
+            self.spd = round(potential_spd * 0.621371, 2)
 
-    	potential_rpm = self.myobd.query(obd.commands.RPM).value
-        potential_rpm = 0 if potential_rpm == None
-        self.rpm = potential_rpm
+        potential_rpm = self.myobd.query(obd.commands.RPM).value
+        if potential_rpm == None:
+            self.rpm = 0
+        else:
+            self.rpm = int(potential_rpm)
 
         potential_maf = self.myobd.query(obd.commands.MAF).value
-    	potential_maf = 1 if potential_maf == None # no division by 0
+        if potential_maf == None:
+            potential_maf = 1
+
         # instant fuel rate
         self.fuel_rate = round(self.spd * (7.718 / potential_maf), 2)
 
         potential_coolant_tmp = self.myobd.query(obd.commands.COOLANT_TEMP).value
-        potential_coolant_tmp = 0 if potential_coolant_tmp == None
-        self.coolant_tmp = potential_coolant_tmp
+        if potential_coolant_tmp == None:
+            self.coolant_tmp = 0
+        else:
+            self.coolant_tmp = int(potential_coolant_tmp)
 
         potential_intake_tmp = self.myobd.query(obd.commands.INTAKE_TEMP).value
-        potential_intake_tmp = 0 if potential_intake_tmp == None
-        self.intake_tmp = potential_intake_tmp
+        if potential_intake_tmp == None:
+            self.intake_tmp = 0
+        else:
+            self.intake_tmp = int(potential_intake_tmp)
 
         potential_fuel_level = self.myobd.query(obd.commands.FUEL_LEVEL).value
-        potential_fuel_level = 0 if potential_fuel_level == None
-        self.fuel_level = potential_fuel_level
+        if potential_fuel_level == None:
+            self.fuel_level = 0
+        else:
+            self.fuel_level = int(potential_fuel_level)
 
 class DashboardApp(App):
     def build(self):
